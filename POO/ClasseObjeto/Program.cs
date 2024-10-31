48ebfe62098db8e51aef8c9540bdf02319f9b593
Rikelme <12rikelmessilva02@gmail.com>
2024-10-31 16:46:02 -0300

    Atualizacao

---
+++ POO/ClasseObjeto/Program.cs
@@ -0,0 +1,39 @@
+﻿using ClasseObjeto.Classes;
+
+Animal cachorro = new Animal ();
+Animal peixe = new Animal ();
+
+peixe.nome= "Pirarucu";
+peixe.especie= "Aquatico";
+peixe.cor= "Vermelho e cinza";
+peixe.idade= 7;
+
+
+cachorro.nome =  "Spike";
+cachorro.especie = "Vira lata";
+cachorro.cor = "Carramelo";
+cachorro.idade = 5;
+
+
+Console.WriteLine($"");
+Console.WriteLine($"Nome do meu Dognho {cachorro.nome} ");
+Console.WriteLine($"Raca do meu Dognho {cachorro.especie} ");
+Console.WriteLine($"Cor do meu Dognho {cachorro.cor} ");
+Console.WriteLine($"Idade do meu Dognho {cachorro.idade} ");
+Console.WriteLine($"");
+Console.WriteLine($"");
+
+Console.WriteLine($"Nome do meu Peixe e {peixe.nome} ");
+Console.WriteLine($"Raca do meu peixe e {peixe.especie} ");
+Console.WriteLine($"Cor do meu peixe e {peixe.cor} ");
+Console.WriteLine($"Idade do meu peixe e {peixe.idade} ");
+Console.WriteLine($"");
+
+Console.WriteLine(@"
+*=================================*
+|              Fim :)             |
+*=================================*
+
+
+
+");
